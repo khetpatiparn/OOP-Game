ad74f7dfaef2d9feb829165c4c85ed7c8cbc7be2
khetpatiparn <86398502+khetpatiparn@users.noreply.github.com>
2024-02-19 13:15:22 +0700

    Delete Game2 directory

--- Game2/TestDialog.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-public class TestDialog {
-    public static void main(String[] args) {
-        String instr = JOptionPane.showInputDialog(null, "Ask for user input (return a String)", "Input Dialog",JOptionPane.PLAIN_MESSAGE);
-        System.out.println("You have entered " + instr);
-    }
-}
