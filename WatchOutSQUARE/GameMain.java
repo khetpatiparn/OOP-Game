fb74b8cd971009929cfaecd04c0f43902b084648
khetpatiparn <86398502+khetpatiparn@users.noreply.github.com>
2024-02-19 13:36:03 +0700

    Update and rename Game.java to GameMain.java

--- WatchOutSQUARE/GameMain.java
+++ WatchOutSQUARE/GameMain.java
@@ -1,6 +1,6 @@
 import javax.swing.SwingUtilities;
 
-public class Game {
+public class GameMain {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
         @Override
